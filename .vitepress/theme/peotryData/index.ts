0a1d74abd6710c2712498a7a9e7b550302bb89a2
心水喵论 <183627391@qq.com>
2023-02-19 09:54:25 +0800

    opt peotrydata order

--- .vitepress/theme/peotryData/index.ts
+++ .vitepress/theme/peotryData/index.ts
@@ -1,7 +1,7 @@
-import aestheticism from "./aestheticism";
-import blue from "./blue";
-import impassioned from "./impassioned";
-import philosophy from "./philosophy";
+import aestheticism from "./aestheticism"; // 唯美
+import impassioned from "./impassioned"; // 激昂
+import philosophy from "./philosophy"; // 哲理
+import blue from "./blue"; // 忧思
 
 export default [
   ...aestheticism,
